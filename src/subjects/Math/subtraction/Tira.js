62571b01210c449d158172ba8b39f5bf5607c70b
allaniccbpo <alopezl62@miumg.edu.gt>
2024-09-13 10:56:29 -0600

    Refactorizar estilos para evitar comportamientos extraños en dispositivos táctiles y desactivar la selección de texto en la página

--- src/subjects/Math/subtraction/Tira.js
+++ src/subjects/Math/subtraction/Tira.js
@@ -2,6 +2,13 @@ import React from 'react';
 import './Tira.css';  // Importa el archivo de estilos actualizado
 
 const Tira = ({ length, number, color, onDragStart }) => {
+    const handleTouchStart = (e) => {
+        e.dataTransfer = {
+            setData: () => {} // Para evitar errores ya que dataTransfer no está disponible en eventos táctiles
+        };
+        onDragStart(e, length); // Manejar el evento táctil como si fuera un arrastre
+    };
+
     return (
         <div
             draggable
@@ -9,6 +16,7 @@ const Tira = ({ length, number, color, onDragStart }) => {
                 e.dataTransfer.setData('tiraLength', length); // Guardar la longitud en dataTransfer
                 onDragStart(e, length); // Asegurar que se pasa el length al evento onDragStart
             }}
+            onTouchStart={handleTouchStart} // Manejar el tacto
             className={`tira ${color} flex items-center justify-center text-white font-bold`}
             style={{ '--length': length }} // Aseguramos que el tamaño de la tira sea proporcional
         >
