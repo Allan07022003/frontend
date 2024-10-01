ddb7ae380074535964855e375043b51d9a7751e7
allan <alopezl62@miumg.edu.gt>
2024-09-30 22:08:48 -0600

    actualizacion de compotamiendo en moviles

--- src/App.js
+++ src/App.js
@@ -6,38 +6,37 @@ import {
   Navigate,
 } from "react-router-dom";
 // rutas de materias
-import MathDashboard from "./subjects/Math/MathDashboard"; 
-import FractionBoard from "./subjects/Math/Fractions/FractionBoard"; 
-import SubtractionActivity from "./subjects/Math/subtraction/SubtractionActivity"; 
+import MathDashboard from "./subjects/Math/MathDashboard";
+import FractionBoard from "./subjects/Math/Fractions/FractionBoard";
+import SubtractionActivity from "./subjects/Math/subtraction/SubtractionActivity";
 import Activity from "./subjects/Lenguaje/Activity";
 
-import { AssistantProvider } from "./context/AssistantContext"; 
-import { Toaster } from "react-hot-toast"; 
+import { AssistantProvider } from "./context/AssistantContext";
+import { Toaster } from "react-hot-toast";
 
 // Importaciones necesarias para React DnD
 import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
 import { TouchBackend } from "react-dnd-touch-backend";
-import { isMobile } from "react-device-detect"; 
 
 // Importaciones para el sistema de autenticación
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
-import ProtectedRoute from "./utils/ProtectedRoute"; 
-import AdminDashboard from "./pages/adminDashboard"; 
-import ResetPassword from "./pages/ResetPassword"; 
-import ResetPasswordForm from "./pages/NewPassword"; 
+import ProtectedRoute from "./utils/ProtectedRoute";
+import AdminDashboard from "./pages/adminDashboard";
+import ResetPassword from "./pages/ResetPassword";
+import ResetPasswordForm from "./pages/NewPassword";
 
 // Nuevas rutas para el módulo de Ciencias
-import CategorySelection from "./subjects/Sciences/CategorySelection"; 
-import BiologiaBasica from "./subjects/Sciences/Pages/BiologiaBasica"; 
-import CicloDeVida from "./subjects/Sciences/Pages/BodyWithOrgans"; 
+import CategorySelection from "./subjects/Sciences/CategorySelection";
+import BiologiaBasica from "./subjects/Sciences/Pages/BiologiaBasica";
+import CicloDeVida from "./subjects/Sciences/Pages/BodyWithOrgans";
 
 function App() {
   return (
     <AssistantProvider>
-<DndProvider backend={isMobile ? TouchBackend({ enableMouseEvents: false, delayTouchStart: 300 }) : HTML5Backend}>
+<DndProvider backend={TouchBackend}>
+
         <Router>
           <div className="min-h-screen bg-gray-100 text-gray-900">
             <Toaster position="bottom-left" reverseOrder={false} />
@@ -51,62 +50,92 @@ function App() {
 
               {/* Ruta para solicitud de restablecimiento de contraseña */}
               <Route path="/reset-password" element={<ResetPassword />} />
-              <Route path="/reset-password/:token" element={<ResetPasswordForm />} />
+              <Route
+                path="/reset-password/:token"
+                element={<ResetPasswordForm />}
+              />
 
               {/* Ruta protegida del dashboard */}
-              <Route path="/dashboard" element={
-                <ProtectedRoute>
-                  <Dashboard />
-                </ProtectedRoute>
-              } />
+              <Route
+                path="/dashboard"
+                element={
+                  <ProtectedRoute>
+                    <Dashboard />
+                  </ProtectedRoute>
+                }
+              />
 
               {/* Rutas para matemáticas */}
-              <Route path="/math" element={
-                <ProtectedRoute>
-                  <MathDashboard />
-                </ProtectedRoute>
-              } />
-              <Route path="/math/fractions" element={
-                <ProtectedRoute>
-                  <FractionBoard />
-                </ProtectedRoute>
-              } />
-              <Route path="/math/subtraction" element={
-                <ProtectedRoute>
-                  <SubtractionActivity />
-                </ProtectedRoute>
-              } />
+              <Route
+                path="/math"
+                element={
+                  <ProtectedRoute>
+                    <MathDashboard />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/math/fractions"
+                element={
+                  <ProtectedRoute>
+                    <FractionBoard />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/math/subtraction"
+                element={
+                  <ProtectedRoute>
+                    <SubtractionActivity />
+                  </ProtectedRoute>
+                }
+              />
 
               {/* Ruta para el módulo de lenguaje */}
-              <Route path="/language" element={
-                <ProtectedRoute>
-                  <Activity />
-                </ProtectedRoute>
-              } />
+              <Route
+                path="/language"
+                element={
+                  <ProtectedRoute>
+                    <Activity />
+                  </ProtectedRoute>
+                }
+              />
 
               {/* Ruta para el panel de administración */}
-              <Route path="/adminDashboard" element={
-                <ProtectedRoute>
-                  <AdminDashboard />
-                </ProtectedRoute>
-              } />
+              <Route
+                path="/adminDashboard"
+                element={
+                  <ProtectedRoute>
+                    <AdminDashboard />
+                  </ProtectedRoute>
+                }
+              />
 
               {/* Rutas para el módulo de Ciencias */}
-              <Route path="/sciences" element={
-                <ProtectedRoute>
-                  <CategorySelection />
-                </ProtectedRoute>
-              } />
-              <Route path="/sciences/biologia-basica" element={
-                <ProtectedRoute>
-                  <BiologiaBasica />
-                </ProtectedRoute>
-              } />
-              <Route path="/sciences/cuerpo-humano" element={
-                <ProtectedRoute>
-                  <CicloDeVida />
-                </ProtectedRoute>
-              } />
+              <Route
+                path="/sciences"
+                element={
+                  <ProtectedRoute>
+                    <CategorySelection />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/sciences/biologia-basica"
+                element={
+                  <ProtectedRoute>
+                    <BiologiaBasica />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/sciences/cuerpo-humano"
+                element={
+                  <ProtectedRoute>
+                    <CicloDeVida />
+                  </ProtectedRoute>
+                }
+              />
             </Routes>
           </div>
         </Router>
